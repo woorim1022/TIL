c758e8f9ea13425fddff4f7ac45de292719ea48a
woorim1022 <woorim1022@naver.com>
2021-03-10 17:52:54 +0900

    Modify IoT/day14 0310.md

--- IoT/kotlinwork/practice/oop/src/kr/multicampus/kotlin/oop/inheritanceTest.kt
+++ IoT/kotlinwork/practice/oop/src/kr/multicampus/kotlin/oop/inheritanceTest.kt
@@ -5,25 +5,48 @@ fun main(){
 }
 
 class Car{
+    val 제조사:String=""
+    val 색상:String=""
     fun 달린다(){
 
     }
     fun 속도올리기(){
 
     }
-    fun 속돌내리기(){
+    fun 속도내리기(){
 
     }
 }
 
 class Bus{
+    val 제조사:String=""
+    val 색상:String=""
+    val 인원수:Int=0
     fun 달린다(){
 
     }
     fun 속도올리기(){
 
     }
-    fun 속돌내리기(){
+    fun 속도내리기(){
+
+    }
+    fun 노선확인하기(){
 
     }
 }
+
+class Truck{
+    val 제조사:String=""
+    val 색상:String=""
+    val 무게:Int=0
+    fun 달린다(){
+
+    }
+    fun 속도올리기(){
+
+    }
+    fun 속도도내리기(){
+    }
+}
+
